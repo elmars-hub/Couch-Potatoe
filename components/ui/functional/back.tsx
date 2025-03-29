8e5e57dd9d2c3cb7df05ac20a7d95e7f0ff0af11
elmars-hub <martinsifeanyi247@gmail.com>
2025-03-29 10:54:59 -0700

    request update

--- components/ui/functional/back.tsx
+++ components/ui/functional/back.tsx
@@ -9,7 +9,7 @@ export default function BackButton() {
   return (
     <button
       onClick={() => router.back()}
-      className="px-4 py-2 rounded-full bg-gray-300 text-black cursor-pointer "
+      className="px-4 py-2 rounded-full bg-gray-200 text-black cursor-pointer "
     >
       <ChevronLeft />
     </button>
